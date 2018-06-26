b767fc468f8ec649428b50b3461ec0b164f2bf9b
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-25 23:08:56 -0700

    Finished basic styling

--- Lab13StudentEnrollment/Models/Course.cs
+++ Lab13StudentEnrollment/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace Lab13StudentEnrollment.Models
     public class Course
     {
         public int ID { get; set; }
+        [Required]
         public string CourseID { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
--- Lab13StudentEnrollment/Models/Student.cs
+++ Lab13StudentEnrollment/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace Lab13StudentEnrollment.Models
     public class Student
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Age { get; set; }
+        [Required]
         public string CourseID { get; set; }
     }
 }
